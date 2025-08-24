7fda325de53239153f8f59050ccb1e8d358cde6d
SuporteCavaloDado <suporte.cavalodado@gmail.com>
2025-08-24 19:11:02 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -231,12 +231,6 @@ function inicializarConfiguracoes() {
 }
 
 // Salvar Endereço
-const ESTADOS_BRASIL = [
-    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 
-    'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 
-    'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
-];
-
 async function salvarEndereco(event) {
     event.preventDefault();
     console.log('Salvando endereço...');
